38d7cee7a76c812119a590db716df722fb3cbdf5
NicklasKleemann <71381595+NicklasKleemann@users.noreply.github.com>
2023-06-05 05:37:59 +0200

    wasd

---
+++ public/javascripts/searchscript.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const searchInput = document.getElementById('searchinput');
+  const searchButton = document.getElementById('searchbutton');
+  const itemList = document.querySelectorAll('#List li');
+
+  function filterItems() {
+    const searchTerm = searchInput.value.toLowerCase();
+
+    itemList.forEach(function(itemLi) {
+      const itemTitle = itemLi.querySelector('a').textContent.toLowerCase();
+      let authorName = '';
+      let bookStatus = '';
+
+      // Check if author element exists
+      const authorElement = itemLi.querySelector('.author');
+      if (authorElement) {
+        authorName = authorElement.textContent.toLowerCase();
+      }
+
+      // Check if status element exists
+      const statusElement = itemLi.querySelector('span');
+      if (statusElement) {
+        bookStatus = statusElement.textContent.toLowerCase();
+      }
+
+      const combinedText = itemTitle + ' ' + authorName + ' ' + bookStatus;
+
+      if (combinedText.includes(searchTerm)) {
+        itemLi.style.display = 'list-item';
+      } else {
+        itemLi.style.display = 'none';
+      }
+    });
+  }
+
+  function goToElement() {
+    const searchTerm = searchInput.value.toLowerCase();
+    let itemURL = '';
+
+    itemList.forEach(function(itemLi) {
+      const itemTitle = itemLi.querySelector('a').textContent.toLowerCase();
+      let authorName = '';
+      let bookStatus = '';
+
+      // Check if author element exists
+      const authorElement = itemLi.querySelector('span');
+      if (authorElement) {
+        authorName = authorElement.textContent.toLowerCase();
+      }
+
+      // Check if status element exists
+      const statusElement = itemLi.querySelector('span');
+      if (statusElement) {
+        bookStatus = statusElement.textContent.toLowerCase();
+      }
+
+      const combinedText = itemTitle + ' ' + authorName + ' ' + bookStatus;
+
+      if (combinedText.includes(searchTerm)) {
+        itemURL = itemLi.querySelector('a').getAttribute('href');
+      }
+    });
+
+    if (itemURL) {
+      location.href = itemURL;
+    }
+  }
+
+  searchInput.addEventListener('input', filterItems);
+  searchButton.addEventListener('click', goToElement);
+});
